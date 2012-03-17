d563e4997cbf0148f85307c7e5ca6563a0c1aaf9
Per Carlsson <perca944@gmail.com>
2012-03-17 12:00:42 +0100

    test

---
+++ poker_iPhone/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  poker_iPhone
+//
+//  Created by Per Carlsson on 3/17/12.
+//  Copyright (c) 2012 Netlight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ poker_iPhone/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  poker_iPhone
+//
+//  Created by Per Carlsson on 3/17/12.
+//  Copyright (c) 2012 Netlight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
